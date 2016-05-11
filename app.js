b23f72c0dd7f93b1a9b4580815059032d68a2988
daoud <daoud@ensta.fr>
2016-05-11 09:19:36 +0200

    Ajout de socket.io --- Création de index.html côté client. Intégration de socket.io coté serveur d'après l'exemple de la documentation.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const http = require('http')
+const fs = require('fs')
 
-const port = 3000
+// const hostname = '127.0.0.1'
+const port = 8080
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  fs.readFile('index.html', 'utf-8', function (err, content) {
+    if (err) { throw err }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(content)
+  })
+})
+
+var io = require('socket.io')(server)
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' })
+  socket.on('my other event', function (data) {
+    console.log(data)
+  })
 })
 
 server.listen(port)
