809c61acff7fc99cc4cd3428f74bf1dfe821f4e1
daoud <daoud@ensta.fr>
2016-05-11 09:43:28 +0200

    Ajout de Twitter --- Intégration de Twitter d'après l'exemple de stream (https://www.npmjs.com/package/twitter) Retour dans la console de tous les tweets traqués via la variable tracking.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const Twitter = require('twitter')
 
 // const hostname = '127.0.0.1'
 const port = 8080
+
+var tracking = '#flash'
+var client = new Twitter({
+  // Not that safe, to improve
+  consumer_key: 'gBW03vbufn6LeLVPBNAQgvFpo',
+  consumer_secret: 'zjMb44NkpjSAU5ZuJkHNoLbd6fVrjDZd4EINbjBdrTYmNJ0Mru',
+  access_token_key: '566644710-yVwCKSsJV5H23ktftUCYkzv4XHXSQx2Dd5l1fBK1',
+  access_token_secret: 'MQxvhiUg07JnHnGC5deOl5YF3JweHjmau1L5h1sXCFhVC'
+})
 
 const server = http.createServer((req, res) => {
   fs.readFile('index.html', 'utf-8', function (err, content) {
@@ -16,9 +26,14 @@ const server = http.createServer((req, res) => {
 var io = require('socket.io')(server)
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
+  client.stream('statuses/filter', {track: tracking}, function (stream) {
+    console.log('User logged !')
+    stream.on('data', function (tweet) {
+      console.log(tweet.text)
+    })
+    stream.on('error', function (error) {
+      throw error
+    })
   })
 })
 
